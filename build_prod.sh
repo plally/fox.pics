516a62ae96cca7e1acb765fb7ffe09f546395dd2
plally <11771225+plally@users.noreply.github.com>
2021-01-27 18:20:34 -0500

    add public directory

--- build_prod.sh
+++ build_prod.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-elm make src/Main.elm --output elm.js --optimize
-uglifyjs elm.js --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters=true,keep_fargs=false,unsafe_comps=true,unsafe=true,passes=2' --output elm.js && uglifyjs elm.js --mangle --output elm.js
+mkdir public
+cp index.html public/index.html
+
+elm make src/Main.elm --output public/elm.js --optimize
+uglifyjs elm.js --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters=true,keep_fargs=false,unsafe_comps=true,unsafe=true,passes=2' --output public/elm.js && uglifyjs elm.js --mangle --output public/elm.js
 
