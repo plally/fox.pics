49aa2bf816a0422023515db6066220aac947fba2
plally <11771225+plally@users.noreply.github.com>
2021-01-27 19:00:50 -0500

    test

--- build_prod.sh
+++ build_prod.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 rm -rf public
-mkdir -p public/js
+mkdir -p public
 cp index.html public/index.html
 
-elm make src/Main.elm --output public/js/main.js --optimize
-uglifyjs public/js/main.js --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters=true,keep_fargs=false,unsafe_comps=true,unsafe=true,passes=2' --output public/js/main.js && uglifyjs public/js/main.js --mangle --output public/js/main.js
+elm make src/Main.elm --output public/main.js --optimize
+uglifyjs public/main.js --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters=true,keep_fargs=false,unsafe_comps=true,unsafe=true,passes=2' --output public/main.js && uglifyjs public/main.js --mangle --output public/main.js
 
