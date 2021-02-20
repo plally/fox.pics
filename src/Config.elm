c3d961d9b5909c74d4cb357cbdb755d5963210ae
plally <11771225+plally@users.noreply.github.com>
2021-02-20 18:32:03 -0500

    fix base url

--- src/Config.elm
+++ src/Config.elm
@@ -1,5 +1,5 @@
 module Config exposing (apiEndpoint)
-baseUrl = "https://store.fox.pics"
+baseUrl = "https://vtt20ior3c.execute-api.us-east-2.amazonaws.com"
 
 apiEndpoint : String -> String
 apiEndpoint path =
