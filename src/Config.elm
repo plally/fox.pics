e4e81184e9778d4ca87c6aee34e688938345e986
plally <11771225+plally@users.noreply.github.com>
2021-02-20 18:20:34 -0500

    change base url

--- src/Config.elm
+++ src/Config.elm
@@ -1,5 +1,5 @@
 module Config exposing (apiEndpoint)
-baseUrl = "https://vtt20ior3c.execute-api.us-east-2.amazonaws.com"
+baseUrl = "https://store.fox.pics"
 
 apiEndpoint : String -> String
 apiEndpoint path =
